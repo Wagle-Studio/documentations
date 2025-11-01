6ceefb755fab5671c3926a9aef954ff85f8b7842
Kew <wolff.kevin@protonmail.com>
2025-11-01 17:54:34 +0100

    Add syntaxe highlight

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,24 @@
-import "./globals.scss";
+import "./../ui/globals.scss";
 import "primereact/resources/primereact.min.css";
 import "primereact/resources/themes/saga-blue/theme.css";
 import type { Metadata } from "next";
+import { Inter, Rubik } from "next/font/google";
 import { PrimeReactProvider } from "primereact/api";
 import MenuManager from "@/core/MenuManager";
 import { MenuSide } from "@/ui/organismes/menu-side/MenuSide";
+
+const rubik = Rubik({
+  subsets: ["latin"],
+  display: "swap",
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-primary",
+});
+
+const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-secondary",
+});
 
 export const metadata: Metadata = {
   title: "Documentations",
@@ -19,7 +33,7 @@ export default function RootLayout({
   const menu = MenuManager.buildMenuSide();
 
   return (
-    <html lang="fr">
+    <html lang="fr" className={`${rubik.variable} ${inter.variable}`}>
       <body>
         <main className="app">
           <MenuSide menu={menu} />
