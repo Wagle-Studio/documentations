791b73b37097c07c08555bae0ca7cfb7d5dbaf20
Kew <wolff.kevin@protonmail.com>
2025-11-01 02:36:28 +0100

    Clear project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,5 @@
-'use client'
-
-import styles from "./page.module.css";
+"use client";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-
-      </main>
-    </div>
-  );
+  return <main></main>;
 }
