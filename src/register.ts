635ab0321d5fc664e29bb49b3303533499c09fbe
Kew <wolff.kevin@protonmail.com>
2025-11-03 01:11:28 +0100

    Documentation : add Docker documents

--- src/register.ts
+++ src/register.ts
@@ -3,9 +3,9 @@ import { Course, Lesson, Register } from "./core/types";
 const lessons: Lesson[] = [
   {
     id: 1,
-    paths: ["apache", "commandes_utiles.mdx"],
+    paths: ["apache", "apache_commandes_utiles.mdx"],
     label: "Commandes utiles",
-    slug: "commandes_utiles",
+    slug: "apache_commandes_utiles",
     course_id: 1,
     created: "2 novembre 2025",
     updated: "2 novembre 2025",
@@ -37,6 +37,51 @@ const lessons: Lesson[] = [
     created: "2 novembre 2025",
     updated: "2 novembre 2025",
   },
+  {
+    id: 5,
+    paths: ["docker", "docker_commandes_utiles.mdx"],
+    label: "Commandes utiles",
+    slug: "docker_commandes_utiles",
+    course_id: 2,
+    created: "2 novembre 2025",
+    updated: "2 novembre 2025",
+  },
+  {
+    id: 6,
+    paths: ["docker", "les_images.mdx"],
+    label: "Les images",
+    slug: "les_images",
+    course_id: 2,
+    created: "2 novembre 2025",
+    updated: "2 novembre 2025",
+  },
+  {
+    id: 7,
+    paths: ["docker", "volumes.mdx"],
+    label: "Les volumes",
+    slug: "volumes",
+    course_id: 2,
+    created: "2 novembre 2025",
+    updated: "2 novembre 2025",
+  },
+  {
+    id: 8,
+    paths: ["docker", "docker_file.mdx"],
+    label: "Dockerfile",
+    slug: "docker_file",
+    course_id: 2,
+    created: "2 novembre 2025",
+    updated: "2 novembre 2025",
+  },
+  {
+    id: 9,
+    paths: ["docker", "docker_compose.mdx"],
+    label: "Docker Compose",
+    slug: "docker_compose",
+    course_id: 2,
+    created: "2 novembre 2025",
+    updated: "2 novembre 2025",
+  },
 ];
 
 const courses: Course[] = [
@@ -45,6 +90,12 @@ const courses: Course[] = [
     label: "Apache",
     slug: "apache",
     emoji: "/emoji/feather.png",
+  },
+  {
+    id: 2,
+    label: "Docker",
+    slug: "docker",
+    emoji: "/emoji/whale.png",
   },
 ];
 
